f3119e0c51bf6b0cdb144b10c16a29e136d41c16
elliottjjackson <ejay01@gmail.com>
2021-03-01 21:22:00 +0800

    Added Linux & Windows functionality

--- time-recorder.py
+++ time-recorder.py
@@ -1,10 +1,17 @@
 from datetime import datetime
+import platform
 
 now = datetime.now()
 now_str = now.strftime("%d/%m/%y %H:%M:%S")
 print("date and time : ",now_str)
+osName = platform.system()
 
-with open('D:/my-documents/code/python/time-recorder/text-store.txt',"a+") as file_object:
+if osName == "Linux":
+    dirFolder = '/home/pi/code/time-recorder/text-store.txt'
+else:
+    dirFolder = 'D:/my-documents/code/python/time-recorder/text-store.txt'
+
+with open(dirFolder,"a+") as file_object:
     file_object.seek(0)
     data = file_object.read(100)
     if len(data) > 0 :
